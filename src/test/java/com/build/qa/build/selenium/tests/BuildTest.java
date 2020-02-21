08ad53eed0153b2040f416c687d57bce99bd54a4
First Last <billysteffenhagen@gmail.com>
2020-02-21 14:23:34 -0800

    Made updates to assessment

--- src/test/java/com/build/qa/build/selenium/tests/BuildTest.java
+++ src/test/java/com/build/qa/build/selenium/tests/BuildTest.java
@@ -1,67 +1,72 @@
 package com.build.qa.build.selenium.tests;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.build.qa.build.selenium.framework.BaseFramework;
 import com.build.qa.build.selenium.pageobjects.homepage.HomePage;
 
-public class BuildTest extends BaseFramework { 
-	
-	/** 
+public class BuildTest extends BaseFramework {
+
+	/**
 	 * Extremely basic test that outlines some basic
 	 * functionality and page objects as well as assertJ
 	 */
 	@Test
-	public void navigateToHomePage() { 
+	public void navigateToHomePage() {
 		driver.get(getConfiguration("HOMEPAGE"));
 		HomePage homePage = new HomePage(driver, wait);
-		
+
 		softly.assertThat(homePage.onBuildTheme())
 			.as("The website should load up with the Build.com desktop theme.")
 			.isTrue();
 	}
-	
-	/** 
+
+	/**
 	 * Search for the Quoizel MY1613 from the search bar
 	 * @assert: That the product page we land on is what is expected by checking the product title
 	 * @difficulty Easy
 	 */
+	@Ignore
 	@Test
-	public void searchForProductLandsOnCorrectProduct() { 
+	public void searchForProductLandsOnCorrectProduct() {
 		// TODO: Implement this test
 	}
-	
-	/** 
-	 * Go to the Bathroom Sinks category directly (https://www.build.com/bathroom-sinks/c108504) 
+
+	/**
+	 * Go to the Bathroom Sinks category directly (https://www.build.com/bathroom-sinks/c108504)
 	 * and add the second product on the search results (Category Drop) page to the cart.
 	 * @assert: the product that is added to the cart is what is expected
 	 * @difficulty Easy-Medium
 	 */
+	@Ignore
 	@Test
-	public void addProductToCartFromCategoryDrop() { 
+	public void addProductToCartFromCategoryDrop() {
 		// TODO: Implement this test
 	}
-	
-	/** 
-	 * Add a product to the cart and email the cart to yourself, also to my email address: jgilmore+SeleniumTest@build.com
+
+	/**
+	 * Add a product to the cart and email the cart to yourself, also to my email address: test.automation+SeleniumTest@build.com
 	 * Include this message in the "message field" of the email form: "This is {yourName}, sending you a cart from my automation!"
 	 * @assert that the "Cart Sent" success message is displayed after emailing the cart
 	 * @difficulty Medium-Hard
 	 */
+	@Ignore
 	@Test
-	public void addProductToCartAndEmailIt() { 
+	public void addProductToCartAndEmailIt() {
 		// TODO: Implement this test
 	}
-	
-	/** 
+
+	/**
 	 * Go to a category drop page (such as Bathroom Faucets) and narrow by
 	 * at least two filters (facets), e.g: Finish=Chromes and Theme=Modern
 	 * @assert that the correct filters are being narrowed, and the result count
 	 * is correct, such that each facet selection is narrowing the product count.
 	 * @difficulty Hard
 	 */
+	@Ignore
 	@Test
-	public void facetNarrowBysResultInCorrectProductCounts() { 
+	public void facetNarrowBysResultInCorrectProductCounts() {
 		// TODO: Implement this test
 	}
 }
